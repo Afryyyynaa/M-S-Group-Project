f06cc0f7a926e75fbc33225e1b4226afafb514dd
Afryyyynaa <108716990+Afryyyynaa@users.noreply.github.com>
2023-10-22 15:50:41 +0800

    Update and rename Player to Player.cs

--- Player.cs
+++ Player.cs
@@ -16,9 +16,17 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * Time.deltaTime;
 
+        float moveDistance = moveSpeed * deltaTime;
+        float playerSize = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.posiiton, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (canMove){
+        transform.position += moveDir * moveDistance;
+        
         isWalking = moveDir != Vector3.zero;
+        
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
